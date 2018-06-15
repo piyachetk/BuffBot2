b2f32614d131666599a5b990006ec3bf7c5268fb
root <root@buffalolarity.com>
2018-06-16 00:18:53 +0700

    Basics

--- app.py
+++ app.py
@@ -1,13 +1,15 @@
+# -*- coding: utf-8 -*-
+
 from flask import Flask, render_template, request
 from chatterbot import ChatBot
 from chatterbot.trainers import ChatterBotCorpusTrainer
 
 app = Flask(__name__)
 
-english_bot = ChatBot("Chatterbot", storage_adapter="chatterbot.storage.SQLStorageAdapter")
+english_bot = ChatBot("BuffBot", storage_adapter="chatterbot.storage.SQLStorageAdapter")
 
-english_bot.set_trainer(ChatterBotCorpusTrainer)
-english_bot.train("chatterbot.corpus.english")
+#english_bot.set_trainer(ChatterBotCorpusTrainer)
+#english_bot.train("chatterbot.corpus.english")
 
 
 @app.route("/")
@@ -16,9 +18,9 @@ def home():
 
 @app.route("/get")
 def get_bot_response():
-    userText = request.args.get('msg')
-    return str(english_bot.get_response(userText))
+    userText = request.args.get('msg').encode("utf-8")
+    return unicode(english_bot.get_response(userText))
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(host='0.0.0.0', port=8080)
